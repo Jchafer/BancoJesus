5bf92a90cd112dddc3c9d348beb19ad005c076e5
Jesus <jchafer@gmail.com>
2020-10-17 17:33:23 +0200

    Diseño terminado v2

--- app/src/main/java/practicas/simarro/bancojesus/MainActivity.java
+++ app/src/main/java/practicas/simarro/bancojesus/MainActivity.java
@@ -2,13 +2,28 @@ package practicas.simarro.bancojesus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageButton botonAcceder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botonAcceder = (ImageButton) findViewById(R.id.btAcceder);
+
+        botonAcceder.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(view.getContext(), AccesoActivity.class);
+        startActivity(intent);
     }
 }
