fa4166c9ed994c15bdab52d98a084ce76142fa09
Jesus <jchafer@gmail.com>
2020-10-17 17:00:41 +0200

    Diseño terminado v1

---
+++ app/src/main/java/practicas/simarro/bancojesus/AccesoActivity.java
@@ -0,0 +1,14 @@
+package practicas.simarro.bancojesus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccesoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceso);
+    }
+}
---
+++ app/src/main/java/practicas/simarro/bancojesus/PrincipalActivity.java
@@ -0,0 +1,14 @@
+package practicas.simarro.bancojesus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
